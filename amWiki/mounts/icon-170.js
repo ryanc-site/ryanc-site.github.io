e8bc90246f388c82decffbb745197768f42dc01a
Ryan <ryanc.site@gmail.com>
2022-01-18 22:02:52 +0800

    delete test dir

--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1642514154526}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1642514536573}
